2d3e5089903853c30ac4e99564fbf7b69db2bdc1
Preston <klpreston@email.neit.edu>
2022-07-20 10:16:48 -0400

    yesm

---
+++ review_se116/review.py
@@ -0,0 +1,35 @@
+#Week 1 Day 2 - SE116 Review Demo
+
+print("\n\n\tWelcome to the Room Safety Manager!\n\n")
+
+answer = input("\tWould you like to check some rooms? (y/n): ").lower()
+
+while answer != "y" and answer != "n":
+
+    print("\t\t**ERROR! INVALID ENTRY!**")
+    answer = input("\tWould you like to check some rooms? (y/n): ").lower()
+
+while answer == "y":
+
+    roomCapacity = int(input("\tEnter the room's max capacity: "))
+
+    peopleAttending = int(input("\tEnter the number of meeting attendees for the room: "))
+
+    difference = roomCapacity - peopleAttending
+
+    if difference >= 0:
+        print("\t\tSAFELY UNDER ROOM CAPACITY")
+        print("\tYou may add up to {0} people to the meeting.".format(difference))
+    
+    else:
+        print("\t\tOH NO! YOU ARE OVER ROOM CAPACITY")
+        print("\tPlease remove at least {0} people from the meeting for safety regulations.".format(difference * -1))
+
+    answer = input("\tWould you like to check some more rooms? (y/n): ").lower()
+
+    while answer != "y" and answer != "n":
+
+        print("\t\t**ERROR! INVALID ENTRY!**")
+        answer = input("\tWould you like to check some rooms? (y/n): ").lower()
+
+print("\n\n\tThank you for using my program! Goodbye! :] \n\n")
